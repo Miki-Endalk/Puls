c1a779bfaed5e5005fcdee575a251c2f70e7b3ba
Mikyas Endalkachew <mikyasendalkachew037@gmail.com>
2026-09-15 21:23:31 +0300

    Set up mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import type { Express, Request, Response } from 'express'
+import { connectDB } from './lib/db.js'
 
 import env from './config/env.js'
 import authRoutes from './routes/auth.route.js'
@@ -11,4 +12,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
+    connectDB()
 })
